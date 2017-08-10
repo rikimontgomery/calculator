4ac200e3451c6a01de616d5e6affcb73a3bdc163
Riki Montgomery <mostriki820@gmail.com>
2017-08-09 21:45:16 -0700

    Update js.

--- js/scripts.js
+++ js/scripts.js
@@ -1,17 +1,17 @@
-var add = function(num1, num2) {
-	return num1 + num2;
+var add = function(number1, number2) {
+	return number1 + number2;
 }
-var subtract = function(num1, num2) {
-	return num1 - num2;
+var subtract = function(number1, number2) {
+	return number1 - number2;
 }
-var multiply = function(num1, num2) {
-	return num1 * num2;
+var multiply = function(number1, number2) {
+	return number1 * number2;
 }
-var divide = function(num1, num2) {
-	return num1 / num2;
+var divide = function(number1, number2) {
+	return number1 / number2;
 }
 
-var one_num = parseInt(prompt("Enter a number: "));
-var two_num = parseInt(prompt("Enter an additional number: "));
+var one_number = parseInt(prompt("Enter a number: "));
+var two_number = parseInt(prompt("Enter an additional number: "));
 
 alert(divide(one_num, two_num));
